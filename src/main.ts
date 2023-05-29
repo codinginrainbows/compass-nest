cad5433d81ddba29f1b6b1a5cafd7831346d84f7
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2023-05-29 11:07:07 -0300

    fix: valid pipe bug

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api/v1');
   await app.listen(3006);
 }
