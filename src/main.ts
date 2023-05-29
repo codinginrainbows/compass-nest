55e5a7f739d666076210016234826d8f54e53781
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2023-05-29 11:24:45 -0300

    fix: rm cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
   // app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api/v1');
-  await app.listen(3006);
+  await app.listen(3000);
 }
 bootstrap();
