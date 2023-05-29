12a07869907083b9d642cfe661c59cf5c067dfd1
Gabriel Garcia <gabrielmgarcia96@gmail.com>
2023-05-29 01:11:04 -0300

    feat: add nest basic structure

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(3006);
+}
+bootstrap();
